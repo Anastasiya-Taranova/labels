b63f28b97df109e9ba12cf7b555551e044ca69e7
Anastasiya Taranava <taranova.nas@gmail.com>
2020-12-09 13:00:06 +0300

    added func for labels

--- scripts/consts.py
+++ scripts/consts.py
@@ -5,5 +5,5 @@ _this_file = Path(__file__).resolve()
 DIR_REPO = _this_file.parent.parent.resolve()
 
 DIR_IDEA = (DIR_REPO / ".idea").resolve()
-DIR_SCRIPTS = (DIR_REPO / "scripts").resolve()
+DIR_SCRIPTS = (DIR_REPO / "utils").resolve()
 DIR_SRC = (DIR_REPO / "src").resolve()
--- src/applications/index/views.py
+++ src/applications/index/views.py
@@ -1,9 +1,23 @@
 from django.views.generic import ListView
 
 from applications.index.models import AuthModel
+from applications.index.utils.labels import delete_label
 
 
 class IndexView(ListView):
     model = AuthModel
     context_object_name = "users"
     template_name = "index/index.html"
+
+    # def get_context_data(self, *args, **kwargs):
+    #     results = delete_label()
+    #     if results is None:
+    #         print("ploxo")
+    #     ctx = super().get_context_data(*args, **kwargs)
+    #     ctx.update(
+    #         {
+    #             "all_info": results,
+    #         }
+    #     )
+    #
+    #     return ctx
