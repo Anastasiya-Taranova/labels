2ec790ea28ed3cac701c316685df50eab0643d42
Anastasiya Taranava <taranova.nas@gmail.com>
2020-12-10 11:03:51 +0300

    make format

--- src/project/urls.py
+++ src/project/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("applications.index.urls")),
-    path("sentry-debug/", lambda _r: 1/0),
+    path("sentry-debug/", lambda _r: 1 / 0),
     path("accounts/", include("allauth.urls")),
 ]
